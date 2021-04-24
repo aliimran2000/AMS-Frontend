ffe3ecb2ae6a51c39a8b18304494087906af27df
aliimran2000 <muhammadaliimran2000@gmail.com>
2021-04-24 22:54:29 +0500

    images

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+    
     return (
-        <div >
+        <div style={{ backgroundImage: `url(${background})` }}>
             <Head>
                 <title>AMS</title>
                 <link rel="icon" href="/favicon.ico" />
